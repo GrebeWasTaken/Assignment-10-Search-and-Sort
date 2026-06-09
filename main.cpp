ae7ec66a6f606516628d8dca8f5d30a369cce37a
A Student <student@nowhere.edu>
2026-06-09 22:37:08 +0000

    Jun 9, 2026, 3:37 PM

--- main.cpp
+++ main.cpp
@@ -78,5 +78,5 @@ int main()
 
   clock_t endPt2 = clock();
 
-  cout << "Selection sort took " << static_cast<double>(endPt2 - startPt2) / CLOCKS_PER_SEC << ""
+  cout << "Selection sort took " << static_cast<double>(endPt2 - startPt2) / CLOCKS_PER_SEC << " seconds." << endl'
 }
