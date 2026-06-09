d7b0dcaa8f6123273e69ac46eef358120c8e5197
A Student <student@nowhere.edu>
2026-06-09 23:34:49 +0000

    Jun 9, 2026, 4:34 PM

--- main.cpp
+++ main.cpp
@@ -129,7 +129,9 @@ Region binaryFind(vector<Region>& regions, int id){
 
     if (regions[mid].id == id){
       return regions[mid];
-    } else if (regions[mid])
+    } else if (regions[mid].id < id){
+      
+    }
   }
 }
 
