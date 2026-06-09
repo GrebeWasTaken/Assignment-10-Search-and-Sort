99645a04cf3e79deb5ae4ea4439904bf2adc394d
A Student <student@nowhere.edu>
2026-06-09 22:38:57 +0000

    Jun 9, 2026, 3:38 PM

--- main.cpp
+++ main.cpp
@@ -80,5 +80,7 @@ int main()
 
   cout << "Selection sort took " << static_cast<double>(endPt2 - startPt2) / CLOCKS_PER_SEC << " seconds." << endl;
 
-  cout << "First five regions after sort:"
+  cout << "First five regions after sort:" << endl;
+
+
 }
