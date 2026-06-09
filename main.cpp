e3e7e0ed8e5e8ee1e39c4d957b5aa8d2f47b33f4
A Student <student@nowhere.edu>
2026-06-09 22:11:20 +0000

    Jun 9, 2026, 3:11 PM

--- main.cpp
+++ main.cpp
@@ -15,7 +15,8 @@ void readRegions(vector<Region>& regions){
 
   while (getline(inputFile, line)){
     if (line != ""){
-      Region r =
+      Region r = parseRegion(line);
+      
     }
   }
 }
