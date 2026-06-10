1fbe27c29dab89811ffcdcaa93d5e2eae4d793da
A Student <student@nowhere.edu>
2026-06-10 04:46:18 +0000

    Jun 9, 2026, 9:46 PM

--- main.cpp
+++ main.cpp
@@ -142,7 +142,19 @@ void printPriceRecord(const PriceRecord& p){
   cout << "ID: " << p.regionId << ", Date: " << p.date << ", Price: " << p.price << endl;
 }
 
+void printFirstFiveRegions(vector<Region>& regions){
+  int amountToPrint = 5;
 
+  if (regions.size() < 5){
+    amountToPrint = regions.size();
+  }
+
+  for (int i = 0; i < amountToPrint; i++){
+    printRegion(regions[i]);
+  }
+}
+
+//part 5
 
 int main()
 {
